12b5ca7081ee3363d013f37f4e74841ba182cc44
Veins (password is veins) <veins@instant-veins.car2x.org>
2024-07-09 19:51:49 +0000

    clean up

--- simulations/rcs/run_all.sh
+++ simulations/rcs/run_all.sh
@@ -13,6 +13,7 @@ trap cleanup EXIT
 
 set -e
 
+# data_dir directory collect the processed simulation log file, only retains the coin transaction related log
 data_dir=${1}
 if [[ -z "$data_dir" ]]; then
 	echo "Input the path to save results as the first parameter.\n"
@@ -23,23 +24,28 @@ if [[ ! -d ${data_dir} ]]; then
 	mkdir -p ${data_dir}
 fi
 
-# Scheme2 Scheme3
-# Scheme2_Pi Scheme3_Pi
-#schemes="" 
-# 1 2 3 4
-#numCpuCores=""
+### scheme Options ###
+# Scheme2 Scheme3 Scheme2_Pi Scheme3_Pi
+
+### numCpuCores Options ###
+# 1 2 3 4 ...
+
+### map Options ###
 # intersection_500 intersection_1000 intersection_1500 intersection_max
 # beijing_200 beijing_500 beijing_1000 beijing_1500 beijing_2000
 # paris_100 paris_300 paris_500 paris_700 paris_1000
-#maps=""
-# PerceptionApp AuctionApp GeneralApp
-apps="GeneralApp"
 
-settings=("beijing_500 Scheme3 2" "beijing_500 Scheme3 3" "beijing_1000 Scheme3 1" "beijing_1000 Scheme3 3" "beijing_1500 Scheme2 4")
+### settings format ###
+# map scheme numCpuCores
+# example: "intersection_500 Scheme2 1"
+settings=("intersection_500 Scheme2 1")
 
 config="omnetpp.ini"
 logfile="simulation.log"
+
+# endrecord file records where the simulation ends and the number of successful and failed cases
 endrecord="endrecord.log"
+
 cp ${config} ${config}.bk
 
 for setting in "${settings[@]}"
@@ -48,21 +54,17 @@ do
 	map=${paras[0]}
 	scheme=${paras[1]}
 	numCpuCore=${paras[2]}
-	for app in $apps
-	do
-		echo "Experiment ${scheme} ${map} ${app} ${numCpuCore}"
-		sed -i "s/maps\/beijing_200/maps\/${map}/g" ${config}
-		sed -i "s/\*\*\.numCpuCores = 1/\*\*\.numCpuCores = ${numCpuCore}/g" ${config}
-		opp_run -r 0 -m -u Cmdenv -c "${scheme}_${app}" -n ..:../../src:../../../inet-3.6.6/src:../../../inet-3.6.6/examples:../../../inet-3.6.6/tutorials:../../../inet-3.6.6/showcases:../../../veins/examples/veins:../../../veins/src/veins:../../../veins/subprojects/veins_inet3/src/veins_inet:../../../veins/subprojects/veins_inet3/examples/veins_inet --image-path=../../images:../../../inet-3.6.6/images:../../../veins/images:../../../veins/subprojects/veins_inet3/images -l ../../src/lte -l ../../../inet-3.6.6/src/INET -l ../../../veins/src/veins -l ../../../veins/subprojects/veins_inet3/src/veins_inet ${config}|| {
-		# I found all lte simulate will ends with error (but data is logged correctly), strange...
-		echo "Experiment ${scheme} ${map} ${app} ${numCpuCore} finished at: $(tail -n 1 ${logfile})" >> ${endrecord}
-		}
-		sed -i -n "/\[WARN\]/p" ${logfile} 
-		echo "Succeeded transactions: $(cat ${logfile} | grep -c "succeed" )" >> ${endrecord}
-		echo "Failed transactions: $(cat ${logfile} | grep -c "fail" )" >> ${endrecord}
-		mv ${logfile} ${data_dir}/cv2x_${scheme}_${map}_${app}_${numCpuCore}.log
-		cp ${config}.bk ${config}
-	done
+	echo "Experiment ${scheme} ${map} GeneralApp ${numCpuCore}"
+	sed -i "s/maps\/intersection/maps\/${map}/g" ${config}
+	sed -i "s/\*\*\.numCpuCores = 1/\*\*\.numCpuCores = ${numCpuCore}/g" ${config}
+	opp_run -r 0 -m -u Cmdenv -c "${scheme}_GeneralApp" -n ..:../../src:../../../inet-3.6.6/src:../../../inet-3.6.6/examples:../../../inet-3.6.6/tutorials:../../../inet-3.6.6/showcases:../../../veins/examples/veins:../../../veins/src/veins:../../../veins/subprojects/veins_inet3/src/veins_inet:../../../veins/subprojects/veins_inet3/examples/veins_inet --image-path=../../images:../../../inet-3.6.6/images:../../../veins/images:../../../veins/subprojects/veins_inet3/images -l ../../src/lte -l ../../../inet-3.6.6/src/INET -l ../../../veins/src/veins -l ../../../veins/subprojects/veins_inet3/src/veins_inet ${config}|| {
+	echo "Experiment ${scheme} ${map} GeneralApp ${numCpuCore} finished at: $(tail -n 1 ${logfile})" >> ${endrecord}
+	}
+	sed -i -n "/\[WARN\]/p" ${logfile} 
+	echo "Succeeded transactions: $(cat ${logfile} | grep -c "succeed" )" >> ${endrecord}
+	echo "Failed transactions: $(cat ${logfile} | grep -c "fail" )" >> ${endrecord}
+	mv ${logfile} ${data_dir}/cv2x_${scheme}_${map}_GeneralApp_${numCpuCore}.log
+	cp ${config}.bk ${config}
 done
 
 rm ${config}.bk
